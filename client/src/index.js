90851d101e5dd764fb467c109d79dceae32f3706
Harsh Lakhanpal <harshlakhanpaln@gmail.com>
2020-08-28 15:14:55 +0530

    Add previously used assets

--- client/src/index.js
+++ client/src/index.js
@@ -4,16 +4,16 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
-// import store from "./store";
+import store from "./store";
 
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <BrowserRouter>
-    {/* <Provider store={store}> */}
-    <App />
-    {/* </Provider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
