1f23bdcd7c812cef62b61c7dac1d5e8006fbb5f8
Harsh Lakhanpal <harshlakhanpaln@gmail.com>
2020-08-29 17:42:29 +0530

    Production changes

---
+++ client/build/precache-manifest.4ea078088d63c95ae5237d5a1328f9ae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a90489923e9e38ca76f50416ac4e24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b09f12d43330982852f6",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "3fb335a1786ffed12213",
+    "url": "/static/css/main.89af973f.chunk.css"
+  },
+  {
+    "revision": "b09f12d43330982852f6",
+    "url": "/static/js/2.6110cb3a.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.6110cb3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb335a1786ffed12213",
+    "url": "/static/js/main.18fd5096.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "68ee63aa77dbc2ef1507419c33dc9f99",
+    "url": "/static/media/delete.68ee63aa.svg"
+  },
+  {
+    "revision": "b8dd6818c7a7377dadb4192f9a65dacf",
+    "url": "/static/media/home.b8dd6818.svg"
+  },
+  {
+    "revision": "8ab7e7675a9118d74d5151ad7fa58840",
+    "url": "/static/media/loader.8ab7e767.gif"
+  },
+  {
+    "revision": "e797dbae16edc55498b83e54e63a3761",
+    "url": "/static/media/logout.e797dbae.svg"
+  },
+  {
+    "revision": "eccd1885d0f037ef62662ed47218a280",
+    "url": "/static/media/view-arrow.eccd1885.svg"
+  }
+]);
